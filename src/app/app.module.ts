75bc4850af8b156faf5ebebcbb132ec59fd36d7a
rafaelcb21 <rafaelcb21@gmail.com>
2020-01-03 11:06:45 -0300

    v0.0.0 inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartsModule } from 'ng2-charts';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RadarChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    RouterModule.forRoot([
+      { path: '', component: RadarChartComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
